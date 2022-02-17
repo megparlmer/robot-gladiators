09504d19ec0c2ba63ccec32cb168a58b95989dd3
megparlmer <megparlmer@gmail.com>
2022-02-17 17:33:42 -0500

    fine tuned the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,54 +14,55 @@ var fight = function(enemyName) {
     while(playerHealth > 0 && enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    //if player chooses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-    //subtract value of "playerAttack" from value of "enemyHealth" and use that to update the value of "enemyHealth" variable
-        enemyHealth = enemyHealth - playerAttack;
-        //log a message to console to confirm it worked
-        console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " health remaining."
-        );
 
-    //check enemy's health
-        if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
-        } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        }
-    //subtract value of "enemyAttack" from value of "playerHealth" and use that to update value of "playerHealth" variable
-        playerHealth = playerHealth - enemyAttack;
-    //log a message in console to confirm it worked
-        console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-        );
-
-    //check player's health
-        if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-            break;
-        }
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
-    //if player chooses to skip
-    }   else if (promptFight === "skip" || promptFight === "SKIP") {
-    //confirm player wants to skip
+    //if player picks 'skip' confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        //confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-    //if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        //subtract money from playerMoney for skipping 
-            playerMoney = playerMoney - 2;
+        //if yes (true), leave fight
+        if(confirmSkip) {
+            window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+            //subtract money from from playerMoney for skipping
+            playerMoney = playerMoney - 10;
+            console.log("playerMoney", playerMoney)
+            break;
         }
-    //if no (false), ask a question again by running fight() again
-        else {
-            fight();
+    }
+
+    //remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " health remaining."
+    );
+    
+    //check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + ' has died!');
+
+        //award player money for winning
+        playerMoney = playerMoney + 20;
+
+        //leave while() loop since enemy is dead
+        break;
+    } else {
+        window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+    }
+
+    //remove player's health by subtracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+    );
+
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + ' has died!');
+        //leave while() loop if player is dead
+        break;
+    } else {
+        window.alert(playerName + ' still has ' + playerHealth + ' health left.');
         }
-    }   else {
-        window.alert("You need to choose a valid option. Try again!");
-    } 
     }
 };
 
