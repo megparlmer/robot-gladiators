7db20eb3b0eeb27bba5d17e648216e31453d1c43
megparlmer <megparlmer@gmail.com>
2022-02-17 17:47:29 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,8 +66,18 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+
+        var pickedEnemyName = enemyNames[i];
+
+        enemyHealth = 50;
+
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in the battle! Game Over!");
+    }
 }
+
