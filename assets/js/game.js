64b5041792ac26d10af40ddd3ab2cb87966f71d3
megparlmer <megparlmer@gmail.com>
2022-02-21 12:42:25 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,16 +24,19 @@ var fight = function(enemyName) {
         if(confirmSkip) {
             window.alert(playerName + ' has decided to skip this fight. Goodbye!');
             //subtract money from from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney)
             break;
         }
     }
 
     //remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
     
     //check enemy's health
@@ -50,7 +53,10 @@ var fight = function(enemyName) {
     }
 
     //remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+    
     console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -78,7 +84,7 @@ var startGame = function() {
 
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             fight(pickedEnemyName);
 
@@ -172,6 +178,12 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 
 //start the game when the page loads
