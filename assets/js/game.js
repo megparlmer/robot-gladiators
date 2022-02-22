95441014d10ad20f88d4195eac3382c09a722497
megparlmer <megparlmer@gmail.com>
2022-02-21 19:07:18 -0500

    Fixed skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,42 @@
-
-//beginning fight
-var fight = function(enemy) {
-
-    while(playerInfo.health > 0 && enemy.health > 0) {
-
+//confirming the player wants to play or skip
+var fightOrSkip = function() {
+    //ask player if they'd like to fight or skip using fightOrSkip function
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    //if player picks 'skip' confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
+    //conditional recursive function call
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    //if player picks "skip" confirm then stop the loop
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
         //confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
         //if yes (true), leave fight
-        if(confirmSkip) {
-            window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-            //subtract money from from playerMoney for skipping
-            playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerMoney", playerInfo.money)
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            //subtract money from playerMoney for skipping
+            playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+
+            //return true if player wants to leave
+            return true;
+        } else {
+            return false;
+        }
+    
+    }
+}
+//beginning fight
+var fight = function(enemy) {
+
+    while(playerInfo.health > 0 && enemy.health > 0) {
+        //ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+            //if true leave fight by breaking loop
             break;
         }
     }
@@ -60,7 +80,6 @@ var fight = function(enemy) {
     } else {
         window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
         }
-    }
 };
 
 var startGame = function() {
